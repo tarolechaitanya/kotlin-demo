fdc2bec15db1908787c0196219401599ddab033e
tarolechaitanya <chaitanya.tarole@gmail.com>
2018-01-06 13:14:27 +0530

    Added button click listener and made change to add button in layout

--- app/src/main/java/com/iss247/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/iss247/kotlindemo/MainActivity.kt
@@ -2,18 +2,31 @@ package com.iss247.kotlindemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textView: TextView
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         println("onCreate called")
         setContentView(R.layout.activity_main)
-        textView = findViewById<TextView>(R.id.txtView)
+        textView = findViewById(R.id.txtView)
         textView.setText("Hello Sumit Tarole !!!")
+
+        button = findViewById(R.id.btn)
+        button.setOnClickListener {
+            showToast()
+        }
+
+    }
+
+    private fun showToast() {
+        Toast.makeText(this, "You clicked me bro !!!", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroy() {
