4f89ee59f2b4e744d9f2a8952a847a8e0153a03e
tarolechaitanya <chaitanya.tarole@gmail.com>
2018-01-06 13:04:17 +0530

    Added setText in main activity Added id in layout textview file

--- app/src/main/java/com/iss247/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/iss247/kotlindemo/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.iss247.kotlindemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        println("onCreate called")
         setContentView(R.layout.activity_main)
+        textView = findViewById<TextView>(R.id.txtView)
+        textView.setText("Hello Sumit Tarole !!!")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy called")
     }
 }
